87cb2f6ea5898fc8504d7921b129cbd4d818957a
pine-hill <mat46.cutter.no9@gmail.com>
2020-08-26 19:20:00 +0900

    プルダウンの実装

--- db/migrate/20200826064547_devise_create_rooms.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class DeviseCreateRooms < ActiveRecord::Migration[6.0]
-  def change
-    create_table :rooms do |t|
-      t.string :name,               null: false
-    end
-  end
-end
